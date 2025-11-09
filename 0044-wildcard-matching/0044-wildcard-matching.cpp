eef48be75568406194afd243f2fa685e15253f4d
AagmanBharti <aagmanbharti01@gmail.com>
2025-11-09 11:19:10 +0530

    Time: 51 ms (26.28%), Space: 15.9 MB (50.59%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,27 @@
 class Solution {
-private:
-    bool solve(int ind1,int ind2,string &s1,string &s2,vector<vector<int>> &dp){
-        if(ind1 < 0 && ind2 < 0) return true;
-        if(ind2 < 0 && ind1 >= 0) return false;
-        if(ind1 < 0 && ind2 >= 0){
-            for(int i=0;i<= ind2;i++){
-                if(s2[i] != '*') return false;
-            }
-            return true;
-        }
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-
-        if(s1[ind1] == s2[ind2] || s2[ind2] == '?') return dp[ind1][ind2] = solve(ind1-1,ind2-1,s1,s2,dp);
-        if(s2[ind2] == '*'){
-            return dp[ind1][ind2] = solve(ind1-1,ind2,s1,s2,dp) || solve(ind1,ind2-1,s1,s2,dp);
-        }
-        return dp[ind1][ind2] = false;
-    }
 public:
-    bool isMatch(string s, string p) {
-        int n1 = s.size();
-        int n2 = p.size();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return solve(n1-1,n2-1,s,p,dp);
+    bool isMatch(string text, string pattern) {
+        int n1 = text.size();
+        int n2 = pattern.size();
+        vector<vector<bool>> dp(n1+1, vector<bool>(n2+1, false));
+        dp[0][0] = true;
+        for (int j = 1; j <= n2; j++) {
+            if (pattern[j - 1] == '*') dp[0][j] = dp[0][j - 1];
+        }
+        for (int i = 1; i <= n1; i++) {
+            dp[i][0] = false;
+        }
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (text[i - 1] == pattern[j - 1] || pattern[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pattern[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 };
