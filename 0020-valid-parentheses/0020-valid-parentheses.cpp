0910bfd3b5db2833abc3b374ec84a0763ee1127a
AagmanBharti <aagmanbharti01@gmail.com>
2026-02-10 21:26:57 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.64%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stack;
-        unordered_map<char, char> mapping = {{')', '('}, {']', '['}, {'}', '{'}};
-
-        for (char c : s) {
-            if (mapping.find(c) == mapping.end()) {
-                stack.push(c);
-            } else if (!stack.empty() && mapping[c] == stack.top()) {
-                stack.pop();
+        stack<char> st;
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
             } else {
-                return false;
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if(c == ')' && top != '(') return false;
+                if(c == '}' && top != '{') return false;
+                if(c == ']' && top != '[') return false;
             }
         }
-
-        return stack.empty();        
+        return st.empty();
     }
 };
