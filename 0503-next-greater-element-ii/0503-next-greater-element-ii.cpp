3cc73c65a3da87a879d9a8570ddb0245df6fc516
AagmanBharti <aagmanbharti01@gmail.com>
2026-02-11 20:52:15 +0530

    Time: 5 ms (45.07%), Space: 28.4 MB (59.29%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,20 +5,14 @@ public:
         vector<int>ans(n,-1);
         stack<int>st;
 
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty() && st.top() <= nums[i]){
+        for(int i = 2*n-1;i>=0;i--){
+            while(!st.empty() && st.top() <= nums[i%n]){
                 st.pop();
             }
-            if(!st.empty()) ans[i] = st.top();
-            st.push(nums[i]);
-        }
-        
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty() && st.top() <= nums[i]){
-                st.pop();
+            if(i < n){
+                ans[i] = st.empty() ? -1 : st.top();
             }
-            if(!st.empty()) ans[i] = st.top();
-            st.push(nums[i]);
+            st.push(nums[i%n]);
         }
         return ans;
     }
