6e1b5486f93258eb0403552093f978ccb9526f8d
AagmanBharti <aagmanbharti01@gmail.com>
2026-02-25 20:49:03 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.92%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     string makeGood(string s) {
-        for(int i = 0;i<s.size();i++){
-            if(s[i] - s[i+1] == 32 || s[i] - s[i+1] == -32){
-                s.erase(i,2);
-                i = -1;
+        string res;
+        
+        for(char c : s){
+            if(!res.empty() && abs(res.back() - c) == 32){
+                res.pop_back();
+            }
+            else{
+                res.push_back(c);
             }
         }
-        return s;
+        return res;
     }
 };
