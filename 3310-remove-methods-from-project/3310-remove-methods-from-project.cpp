8fa8c0c1d9b7a98b539aa34148edd35657120e09
AagmanBharti <aagmanbharti01@gmail.com>
2026-08-05 11:42:25 +0530

    Time: 155 ms (91.38%), Space: 301.1 MB (80.69%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> graph(n);
+        vector<int> vis(n);
+
+        for(auto &e : invocations){
+            graph[e[0]].push_back(e[1]);
+        }
+
+        queue<int> q;
+        q.push(k);
+        vis[k] = 1;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(int adj : graph[node]){
+                if(!vis[adj]){
+                    vis[adj] = 1;
+                    q.push(adj);
+                }
+            }
+        }
+
+        for(auto &e : invocations){
+            if(!vis[e[0]] && vis[e[1]]){
+                vector<int> ans;
+                for(int i = 0; i < n; i++){
+                    ans.push_back(i);
+                }
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+        for(int i = 0;i < n;i++){
+            if(!vis[i]) ans.push_back(i);
+        }
+        return ans;
+    }
+};
