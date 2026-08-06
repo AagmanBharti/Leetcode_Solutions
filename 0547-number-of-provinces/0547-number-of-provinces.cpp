a4a492323f41d6ae4471aca49eab58572338521a
AagmanBharti <aagmanbharti01@gmail.com>
2026-08-06 11:49:42 +0530

    Time: 7 ms (11.42%), Space: 20.8 MB (21.75%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-
-    void dfs(int node,vector<vector<int>>& adjLs,vector<int>& vis){
+    void dfs(int node, vector<int>& vis, vector<vector<int>>& adjLs){
         vis[node] = 1;
-        for(auto& it: adjLs[node]){
-            if(!vis[it]) dfs(it,adjLs,vis);
+        for(auto it : adjLs[node]){
+            if(!vis[it]){
+                dfs(it, vis, adjLs);
+            }
         }
     }
 
@@ -12,8 +13,8 @@ public:
         int n = isConnected.size();
         vector<vector<int>> adjLs(n);
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
                 if(isConnected[i][j] == 1 && i != j){
                     adjLs[i].push_back(j);
                     adjLs[j].push_back(i);
@@ -21,14 +22,14 @@ public:
             }
         }
 
+        int count = 0;
         vector<int> vis(n,0);
-        int cnt = 0;
-        for(int i = 0;i<n;i++){
+        for(int i = 0;i < n;i++){
             if(!vis[i]){
-                cnt++;
-                dfs(i,adjLs,vis);
+                count++;
+                dfs(i, vis, adjLs);
             }
         }
-        return cnt;
+        return count;
     }
 };
