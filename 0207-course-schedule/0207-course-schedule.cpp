e7e02c8da8772b3e8ac9b08eaba807f8673478ca
AagmanBharti <aagmanbharti01@gmail.com>
2026-08-08 11:27:34 +0530

    Time: 4 ms (63.43%), Space: 19.1 MB (90.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
         vector<vector<int>> adj(numCourses);
-        for (auto& pre : prerequisites) {
-            adj[pre[0]].push_back(pre[1]); // Edge: pre[0] -> pre[1]
+
+        // Build graph
+        for(auto& it : prerequisites) {
+            adj[it[0]].push_back(it[1]);
         }
 
+        // Calculate indegree
         vector<int> indegree(numCourses, 0);
-        for (int i = 0; i < numCourses; i++) {
-            for (int it : adj[i]) {
+
+        for(int i = 0; i < numCourses; i++) {
+            for(auto it : adj[i]) {
                 indegree[it]++;
             }
         }
 
+        // Courses with indegree 0
         queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0)
-                q.push(i);
-        }
 
-        vector<int> topo;
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for (auto it : adj[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0)
-                    q.push(it);
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
             }
         }
-        return topo.size() == numCourses;
+
+        int count = 0;
+
+        // BFS
+        while(!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            count++;
+
+            for(auto it : adj[node]) {
+                indegree[it]--;
+
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        return count == numCourses;
     }
 };
