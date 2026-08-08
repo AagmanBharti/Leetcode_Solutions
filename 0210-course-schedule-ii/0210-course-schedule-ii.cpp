4891a3754bdb3c492102ffd906849fbfe296b67d
AagmanBharti <aagmanbharti01@gmail.com>
2026-08-08 11:32:11 +0530

    Time: 2 ms (75.56%), Space: 18.3 MB (70.93%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,32 +1,51 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses,vector<int>());
-        vector<int> indegree(numCourses);
-        for(vector<int>&ans : prerequisites){
-            adj[ans[1]].push_back(ans[0]);
-            indegree[ans[0]]++;
+        
+        vector<vector<int>> adj(numCourses);
+
+        // Build graph
+        for(auto& it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
         }
 
+        // Calculate indegree
+        vector<int> indegree(numCourses, 0);
+
+        for(int i = 0; i < numCourses; i++) {
+            for(auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+
+        // Courses with indegree 0
         queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i] == 0) q.push(i);
+
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
         }
 
         vector<int> topo;
-        int vis=0;
-        while(!q.empty()){
+
+        // BFS
+        while(!q.empty()) {
+
             int node = q.front();
             q.pop();
+
             topo.push_back(node);
-            vis++;
-            for(auto it:adj[node]){
+
+            for(auto it : adj[node]) {
                 indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
+
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
             }
         }
-        if(vis == numCourses) return topo;
+        if(topo.size() == numCourses) return topo;
         return {};
-
     }
 };
