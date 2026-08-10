f2301905500089259c411c36cfc5a526d1b090e5
AagmanBharti <aagmanbharti01@gmail.com>
2026-08-10 22:32:29 +0530

    Time: 21 ms (94.46%), Space: 40.2 MB (66.69%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,118 @@
+class DisjointSet {
+    vector<int> rank;
+    vector<int> parent;
+
+public:
+    DisjointSet(int n) {
+        rank.resize(n, 0);
+        parent.resize(n);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v)
+            return;
+
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(
+        vector<vector<string>>& accounts
+    ) {
+
+        int n = accounts.size();
+
+        DisjointSet ds(n);
+
+        unordered_map<string, int> mapMailNode;
+
+        // Step 1: Connect accounts having common emails
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 1; j < accounts[i].size(); j++) {
+
+                string mail = accounts[i][j];
+
+                if (mapMailNode.find(mail) == mapMailNode.end()) {
+
+                    // First time seeing this email
+                    mapMailNode[mail] = i;
+
+                }
+                else {
+
+                    // Email already belongs to another account
+                    ds.unionByRank(
+                        i,
+                        mapMailNode[mail]
+                    );
+                }
+            }
+        }
+
+        // Step 2: Put emails into their component
+        vector<vector<string>> mergedMail(n);
+
+        for (auto& it : mapMailNode) {
+
+            string mail = it.first;
+
+            int node = ds.findUPar(it.second);
+
+            mergedMail[node].push_back(mail);
+        }
+
+        // Step 3: Create answer
+        vector<vector<string>> ans;
+
+        for (int i = 0; i < n; i++) {
+
+            if (mergedMail[i].empty())
+                continue;
+
+            sort(
+                mergedMail[i].begin(),
+                mergedMail[i].end()
+            );
+
+            vector<string> temp;
+
+            // Account name
+            temp.push_back(accounts[i][0]);
+
+            // Emails
+            for (auto& mail : mergedMail[i]) {
+                temp.push_back(mail);
+            }
+
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
