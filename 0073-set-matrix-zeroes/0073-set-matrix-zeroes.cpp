3a533b7993041fee107de3325074bba455c25837
AagmanBharti <aagmanbharti01@gmail.com>
2026-08-11 20:15:51 +0530

    Time: 0 ms (100%), Space: 21.1 MB (22.83%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-    int col0 = 1;
-    int n = matrix.size();
-    int m = matrix[0].size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                // mark i-th row:
-                matrix[i][0] = 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-                // mark j-th column:
-                if (j != 0)
-                    matrix[0][j] = 0;
-                else
-                    col0 = 0;
+        vector<int> row(m,0);
+        vector<int> col(n,0);
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(matrix[i][j] == 0){
+                    row[i] = -1;
+                    col[j] = -1;
+                }
             }
         }
-    }
 
-    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-            if (matrix[i][j] != 0) {
-                // check for col & row:
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(row[i] == -1 || col[j] == -1){
                     matrix[i][j] = 0;
                 }
             }
         }
-    }
-
-    //step 3: Finally mark the 1st col & then 1st row:
-    if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
-            matrix[0][j] = 0;
-        }
-    }
-    if (col0 == 0) {
-        for (int i = 0; i < n; i++) {
-            matrix[i][0] = 0;
-        }
-    }
     }
 };
