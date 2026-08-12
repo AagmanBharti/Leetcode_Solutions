9ee2a2c8f0316145ac396ad52de97b1d83a9e641
AagmanBharti <aagmanbharti01@gmail.com>
2026-08-12 11:42:40 +0530

    Time: 23 ms (53.43%), Space: 17.4 MB (85.6%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,30 +2,49 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
+        vector<vector<int>> ans;
+
         sort(nums.begin(), nums.end());
-        vector<vector<int>> output;
-        for(int i=0; i<n-3; i++){
-            for(int j=i+1; j<n-2; j++){
-                long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
-                int low = j+1, high = n-1;
-                while(low < high){
-                    if(nums[low] + nums[high] < newTarget){
-                        low++;
-                    }
-                    else if(nums[low] + nums[high] > newTarget){
-                        high--;
-                    }
-                    else{
-                        output.push_back({nums[i], nums[j], nums[low], nums[high]});
-                        int tempIndex1 = low, tempIndex2 = high;
-                        while(low < high && nums[low] == nums[tempIndex1]) low++;
-                        while(low < high && nums[high] == nums[tempIndex2]) high--;
-                    }
+
+        for(int i = 0;i < n-3;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            for(int j = i + 1;j < n-2;j++){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while(left < right){
+                    long long sum = (long long)nums[i]
+                                  + nums[j]
+                                  + nums[left]
+                                  + nums[right];
+                    if(sum == target){
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+
+                    while (left < right &&
+                               nums[left] == nums[left + 1])
+                            left++;
+
+                        while (left < right &&
+                               nums[right] == nums[right - 1])
+                            right--;
+
+                        left++;
+                        right--;
                 }
-                while(j+1 < n && nums[j] == nums[j+1]) j++;
+                 else if (sum < target) {
+                        left++;
+                    }
+
+                    else {
+                        right--;
             }
-            while(i+1 < n && nums[i] == nums[i+1]) i++;
+                }
         }
-        return output;
+        }
+
+        return ans;
     }
 };
