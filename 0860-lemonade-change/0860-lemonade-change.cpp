e26b2ad9f855adac801e9b93caedd808f0eb7bf0
AagmanBharti <aagmanbharti01@gmail.com>
2026-08-20 21:56:33 +0530

    Time: 3 ms (10.13%), Space: 87.2 MB (76.08%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int fives = 0;
+        int five = 0;
         int tens = 0;
 
         for (int i = 0; i < bills.size(); i++) {
             if (bills[i] == 5)
-                fives++;
+                five++;
             else if (bills[i] == 10) {
-                if (fives) {
-                    fives--;
+                if (five) {
+                    five--;
                     tens++;
                 } else
                     return false;
             } else {
-                if (fives && tens) {
-                    fives--;
+                if (five && tens) {
+                    five--;
                     tens--;
-                } else if (fives >= 3) {
-                    fives -= 3;
-                } else {
+                } else if (five >= 3) {
+                    five -= 3;
+                } else
                     return false;
-                }
             }
         }
         return true;
