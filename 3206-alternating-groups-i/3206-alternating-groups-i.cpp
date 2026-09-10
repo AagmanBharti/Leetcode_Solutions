fbe7cb096937c00f88428345d57b271427799d9a
AagmanBharti <aagmanbharti01@gmail.com>
2026-09-10 11:12:27 +0530

    Time: 12 ms (11.32%), Space: 27 MB (55.4%) - LeetHub

---
+++ 3206-alternating-groups-i/3206-alternating-groups-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        int ans = 0;
+        int n = colors.size();
+
+        for (int i = 0; i < n; i++) {
+            if (colors[i] == colors[(i + 2) % n] &&
+                colors[i] != colors[(i + 1) % n]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
