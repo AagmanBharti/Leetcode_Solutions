cf4f974d534ac37128a07822d8adcf97a0b05d8d
AagmanBharti <aagmanbharti01@gmail.com>
2026-09-13 11:52:44 +0530

    Time: 0 ms (100%), Space: 73.2 MB (91.11%) - LeetHub

---
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> cyclicShift(int n, vector<vector<int>>& grid,
+                                    vector<int>& rowShift,
+                                    vector<int>& colShift) {
+        vector<vector<int>> ans;
+        ans = grid;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[i][(j - rowShift[i] + n) % n] = grid[i][j];
+            }
+        }
+
+        grid = ans;
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                ans[(i - colShift[j] + n) % n][j] = grid[i][j];
+            }
+        }
+        return ans;
+    }
+};
