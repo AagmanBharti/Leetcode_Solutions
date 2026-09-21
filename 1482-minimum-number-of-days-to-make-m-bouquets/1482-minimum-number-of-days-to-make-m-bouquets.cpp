e5e60b6a19e73704e16e8befb696f4a0c0b4b5c2
AagmanBharti <aagmanbharti01@gmail.com>
2026-09-21 20:25:31 +0530

    Time: 23 ms (52.59%), Space: 70.4 MB (80.46%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-bool possible(vector<int>& bloomDay, int day, int m, int k) {
+    bool possible(vector<int>& bloomDay, int day, int m, int k) {
+        int bouquets = 0;
+        int consecutive = 0;
 
-    int bouquets = 0;
-    int consecutive = 0;
-
-    for(int flower : bloomDay) {
-
-        if(flower <= day) {
-            consecutive++;
-
-            if(consecutive == k) {
-                bouquets++;
+        for (int flower : bloomDay) {
+            if (flower <= day) {
+                consecutive++;
+                if (consecutive == k) {
+                    bouquets++;
+                    consecutive = 0;
+                }
+            } else {
                 consecutive = 0;
             }
         }
-        else {
-            consecutive = 0;
-        }
+        return bouquets >= m;
     }
 
-    return bouquets >= m;
-}
-
     int minDays(vector<int>& bloomDay, int m, int k) {
-
         long long required = 1LL * m * k;
-
-        if(required > bloomDay.size())
+        if (required > bloomDay.size())
             return -1;
 
         int low = *min_element(bloomDay.begin(), bloomDay.end());
@@ -35,19 +28,16 @@ bool possible(vector<int>& bloomDay, int day, int m, int k) {
 
         int ans = -1;
 
-        while(low <= high) {
-
+        while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if(possible(bloomDay, mid, m, k)) {
+            if (possible(bloomDay, mid, m, k)) {
                 ans = mid;
                 high = mid - 1;
-            }
-            else {
+            } else {
                 low = mid + 1;
             }
         }
-
         return ans;
     }
 };
