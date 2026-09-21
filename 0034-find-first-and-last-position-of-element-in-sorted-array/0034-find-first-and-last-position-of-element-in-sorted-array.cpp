2820866fb5c377ec2465e8f0e336d3655cb2e08e
AagmanBharti <aagmanbharti01@gmail.com>
2026-09-21 12:12:01 +0530

    Time: 2 ms (1.39%), Space: 17.6 MB (53.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,49 @@
 class Solution {
 public:
-    int firstIndex(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0, high = n - 1;
+    int firstIdx(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
         int first = -1;
+
         while (low <= high) {
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
+
             if (nums[mid] == target) {
                 first = mid;
-                high = mid - 1;
+                high = mid - 1; // search left
             } else if (nums[mid] > target) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
+
         return first;
     }
 
-    int lastIndex(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0, high = n - 1;
+    int lastIdx(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
         int last = -1;
+
         while (low <= high) {
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
+
             if (nums[mid] == target) {
                 last = mid;
+                low = mid + 1; // search right
+            } else if (nums[mid] < target) {
                 low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
             } else {
-                low = mid + 1;
+                high = mid - 1;
             }
         }
+
         return last;
     }
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = firstIndex(nums, target);
-        if (first == -1)
-            return {-1, -1};
-        int last = lastIndex(nums, target);
+        int first = firstIdx(nums, target);
+        int last = lastIdx(nums, target);
+
         return {first, last};
     }
 };
